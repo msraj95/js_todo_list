c56d9965844bff53597c29e14b2cc25035c3ba0c
Mohammad Seraj <83978726+msraj95@users.noreply.github.com>
2022-03-22 23:40:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -10,4 +10,9 @@ const a;
 a=2;
 console.log(a);
 
-
+//
+function addNode() 
+     {var newP = document.createElement("p"); 
+	  var textNode = document.createTextNode(" This is a new text node"); 
+	  newP.appendChild(textNode);
+      document.getElementById("firstP").appendChild(newP); } 
