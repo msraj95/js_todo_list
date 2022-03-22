73fe607273ff77104571c9ae0ec176a33344d7d6
Mohammad Seraj <83978726+msraj95@users.noreply.github.com>
2022-03-22 23:41:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -16,3 +16,12 @@ function addNode()
 	  var textNode = document.createTextNode(" This is a new text node"); 
 	  newP.appendChild(textNode);
       document.getElementById("firstP").appendChild(newP); } 
+//adding dynamic div using js
+var participantsField = document.getElementById("Participentfieldwrap"),
+        form = document.getElementsByTagName("form")[0],
+        ContestantNum = 4,
+        i;
+    for(i=0; i<ContestantNum; i++){
+      var clone = participantsField.cloneNode(true);
+      form.appendChild(clone);
+    }
